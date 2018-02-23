005f93df484fc9e109a0c2d00307d3690af9dfd8
SarahHale <id15she@cs.umu.se>
2018-02-23 13:24:33 +0100

    place markers

--- src/Map.js
+++ src/Map.js
@@ -6,14 +6,43 @@ import {
   Marker,
 } from "react-google-maps";
 
+//Hardcoded data
+const markers= [{
+    id: '1',
+    latitude:-34.397,
+    longitude:150.644,
+    createdAt: '00:00',
+    updatedAt: '00:00'
+  },
+  {
+    id: '2',
+    latitude:-34.400,
+    longitude:150.644,
+    createdAt: '00:00',
+    updatedAt: '00:00'
+
+  },
+  {
+    id: '3',
+    latitude:-34.397,
+    longitude:150.650,
+    createdAt: '00:00',
+    updatedAt: '00:00'
+
+  },
+]
+
 const MapWithAMarker = withScriptjs(withGoogleMap(props =>
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}
   >
-    <Marker
-      position={{ lat: -34.397, lng: 150.644 }}
-    />
+    {markers.map(marker => (
+      <Marker
+        position={{ lat: marker.latitude, lng: marker.longitude }}
+      />
+    ))}
+
   </GoogleMap>
 ));
 
@@ -42,10 +71,11 @@ export default class Map extends React.Component {
     return (
       <MapWithAMarker
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    )
-  }
+        loadingElement={<div style={{ height: `100%` }} />
+    }
+    containerElement = { <div style={{ height: `400px` }} /> }
+    mapElement = { <div style={{ height: `100%` }} /> }
+    />
+  )
+}
 }
